f8f15a66ddbc96ad5ca5177aea5767663f3b97c1
FranciscoCerqueira412 <fjccerqueira1999@gmail.com>
2022-12-31 20:24:46 +0000

    Add maps

--- Weather/script.js
+++ Weather/script.js
@@ -6,6 +6,11 @@ const humidity = document.querySelector(".humidity-percentage");
 const windVelocity = document.querySelector(".wind-velocity");
 const searchBtn = document.querySelector(".search-box button");
 const weather404 = document.querySelector(".weather-404");
+const country = document.querySelector(".country");
+const streetview=document.querySelector(".streetview iframe");
+const street=document.querySelector(".streetview");
+
+
 
 searchBtn.addEventListener("click", (e) => {
     
@@ -16,21 +21,35 @@ searchBtn.addEventListener("click", (e) => {
 
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${APIkey}`
-    )
+)
         .then((response) => response.json())
         .then((json) => {
             if (json.cod == "404") {
                 container.style.height = "400px";
                 weather404.style.display = "block";
+                country.style.display="none";
+                streetview.style.display="none";
+                weatherImage.style.display="none";
+                street.style.display="none";
                 return;
             }
             console.log(json);
             container.style.height = "600px";
             weather404.style.display = "none";
+            weatherImage.style.display="block";
             description.innerHTML = json.weather[0].description;
             humidity.innerHTML = json.main.humidity +" %";
             windVelocity.innerHTML = Math.round(json.wind.speed) + " Km/h";
             temperature.innerHTML = Math.round(json.main.temp) + "<span>ºC</span> ";
+            country.innerHTML = json.sys.country;
+            country.style.display="block";
+            const mapsLat=json.coord.lat;
+            const mapsLon=json.coord.lon;
+            streetview.style.display="block";
+            street.style.display="block";
+
+            streetview.src="https://www.google.com/maps/embed/v1/view?key=AIzaSyBksX_TcLukUuLk_KlR7dsru24SWb3EBGg&&&center="+mapsLat+","+mapsLon+"&zoom=10&maptype=roadmap"
+
 
             switch (json.weather[0].main) {
                 case "Clear":
